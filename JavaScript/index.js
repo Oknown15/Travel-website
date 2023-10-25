768b7c482a4ec4f6b4c1e3eaed481d87533ff75c
Oknown15 <buchijunior90@gmail.com>
2023-10-25 11:44:26 +0100

    new Push

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -16,37 +16,8 @@ unOrdered.className = ""
 const nav = document.getElementById("nav");
 nav.append(unOrdered);
 
-const Links = document.createElement("a");
-Links.id = "nav1";
-Links.href = "#";
-Links.className = " text-dark ";
-Links.textContent = "Places"
-
-const navOne = document.getElementById("link");
-navOne.appendChild(Links);
-
-const LinksOne = document.createElement("a");
-LinksOne.id = "nav2";
-LinksOne.href = "#";
-LinksOne.className = " text-dark";
-LinksOne.textContent = "Restaurants"
-const navTwo = document.getElementById("link");
-navTwo.appendChild(LinksOne);
-
-const LinksTwo = document.createElement("a");
-LinksTwo.id = "nav3";
-LinksTwo.className = " text-dark"
-LinksTwo.textContent = "Hotels"
-const navThree = document.getElementById("link");
-navThree.append(LinksTwo);
-
-const LinksThree = document.createElement("a");
-LinksThree.id = "nav4";
-LinksThree.className = " text-dark"
-LinksThree.textContent = "Parks"
-const navFour = document.getElementById("link");
-navFour.append(LinksThree);
-
+const navigate = document.getElementById("navigation");
+unOrdered.appendChild(navigate)
 
 const Thefather = document.createElement("div")
 Thefather.className = "container-fluid"
@@ -113,6 +84,135 @@ newDiv.append(imgDescription);
 const moreBtn =document.createElement("a")
 moreBtn.id = "more";
 moreBtn.href = "#";
+moreBtn.onclick = "moreMenu()";
 moreBtn.className = "text-capitalize position-relative"
 moreBtn.textContent = "More";
 Thetext.append(moreBtn)
+
+
+const moreMenu = () => {
+    document.getElementById("Father").style.display= "none";
+    document.getElementById("body").style.backgroundImage = "none"
+    
+    const Fatherdoc1 = document.createElement("div")
+    Fatherdoc1.id = "Fatherdoc"
+    Fatherdoc1.className = "position-relative"
+    document.body.appendChild(Fatherdoc1)
+
+    const docImg = document.createElement("img")
+    docImg.src = "./styles/Images/abuja.jpg";
+    docImg.id = "abujaImg"
+    const Fatherdoc = document.getElementById("Fatherdoc");
+    Fatherdoc.appendChild(docImg)
+
+    const head = document.createElement("h3");
+    head.id = "abujaPop";
+    head.className =  "text-capitalize position-relative"
+    head.textContent = "travellers' choice"
+    const Papa =document.getElementById("Fatherdoc");
+    Papa.appendChild(head);
+
+    const doc = document.createElement("p");
+    doc.id = "document";
+    doc.className = "text-capitalize text-dark p-3"
+    doc.textContent = "abuja to the world -";
+    Papa.append(doc)
+
+    const doc1 = document.createElement("p");
+    doc1.id = "document1";
+    doc1.className = "p-3"
+    doc1.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt accusamus repudiandae optio magnam in fugit tenetur architecto. Laboriosam atque eos fugiat quod. Esse unde a omnis alias dicta accusamus hic!"
+    Papa.appendChild(doc1)
+    
+    document.getElementById("carouselExampleSlidesOnly").style.display = "block";
+
+    const Jabi = document.createElement("code");
+    Jabi.textContent = "01. jabi lake mall";
+    Jabi.className = "p-2 text-capitalize"
+    Papa.appendChild(Jabi);
+
+    const next = document.createElement("p");
+    next.className = "p-5 text-primary firstText"
+    next.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt accusamus repudiandae optio magnam in fugit tenetur architecto. Laboriosam atque eos fugiat quod. Esse unde a omnis alias dicta accusamus hic!"
+    Papa.append(next)
+
+    const Papa1 = document.createElement("div");
+    Papa1.id = "Papa1"
+    document.body.appendChild(Papa1);
+
+    const Pyramid = document.createElement("code")
+    Pyramid.id = "Pyramid"
+    Pyramid.className = "text-capitalize"
+    Pyramid.textContent = "02.  Pyramid art centre, abuja"
+    const Papa1main = document.getElementById("Papa1")
+    Papa1main.appendChild(Pyramid)
+
+    const nextImage = document.createElement("img");
+    nextImage.id = "nextImg";
+    nextImage.src = "./styles/Images/images (35).jpeg";
+    Papa1main.appendChild(nextImage);
+
+    const nextImagedoc = document.createElement("p");
+    nextImagedoc.id = "documentForImg";
+    nextImagedoc.className = "text-primary";
+    nextImagedoc.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt accusamus repudiandae optio magnam in fugit tenetur architecto. Laboriosam atque eos fugiat quod. Esse unde a omnis alias dicta accusamus hic!";
+    Papa1main.append(nextImagedoc);
+
+    const Maitama = document.createElement("code");
+    Maitama.textContent = "03. maitama";
+    Maitama.className = "text-capitalize";
+    Papa1main.appendChild(Maitama);
+
+    const nextImage1 = document.createElement("img");
+    nextImage1.src = "./styles/Images/abuja (38).jpg";
+    nextImage1.className = "nextImage1"
+    Papa1main.appendChild(nextImage1);
+
+    const nextImage1doc = document.createElement("p");
+    nextImage1doc.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt accusamus repudiandae optio magnam in fugit tenetur architecto. Laboriosam atque eos fugiat quod. Esse unde a omnis alias dicta accusamus hic!";
+    nextImage1doc.id = "documentForImg1";
+    nextImage1doc.className = "text-primary"
+    Papa1main.append(nextImage1doc);
+
+    const asokoro = document.createElement("code");
+    asokoro.textContent = "04.  asokoro";
+    asokoro.className = "text-capitalize";
+    Papa1main.appendChild(asokoro)
+
+    const nextImage2 = document.createElement("img");
+    nextImage2.className = "nextImg2"
+    nextImage2.src = "./styles/Images/abuja (39).jpg";
+    Papa1main.appendChild(nextImage2);
+
+    const nextImage21 = document.createElement("img");
+    nextImage21.className ="nextImg21"
+    nextImage21.src = "./styles/Images/abuja (40).jpg";
+    Papa1main.appendChild(nextImage21)
+
+    const nextImage22 = document.createElement("img");
+    nextImage22.className = "nextImg22";
+    nextImage22.src = "./styles/Images/images (38).jpeg";
+    Papa1main.appendChild(nextImage22);
+    
+    const nextImage2doc = document.createElement("p");
+    nextImage2doc.className = "documentForImg2 text-capitalize text-primary"
+    nextImage2doc.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt accusamus repudiandae optio magnam in fugit tenetur architecto. Laboriosam atque eos fugiat quod. Esse unde a omnis alias dicta accusamus hic!";
+    Papa1main.appendChild(nextImage2doc);
+
+    const gwarimpa =document.createElement("code");
+    gwarimpa.textContent = "05. zuma rock";
+    gwarimpa.className = "text-capitalize";
+    Papa1main.appendChild(gwarimpa);
+
+    const nextImage3 = document.createElement("img");
+    nextImage3.className  = "nextImg3"
+    nextImage3.src = "./styles/Images/abuja (41).jpg";
+    Papa1main.appendChild(nextImage3);
+
+    const nextImage3doc = document.createElement("p");
+    nextImage3doc.className = "text-capitalize documentForImg3 text-primary"
+    nextImage3doc.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt accusamus repudiandae optio magnam in fugit tenetur architecto. Laboriosam atque eos fugiat quod. Esse unde a omnis alias dicta accusamus hic!";
+    Papa1main.appendChild(nextImage3doc);
+}
+const menu = document.getElementById("more");
+menu.onclick = moreMenu;
